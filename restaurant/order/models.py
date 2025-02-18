65dd666213fdfd4ca5b65e968b50f0b0d34ef128
BanyarSawThit <banyarsawthit1922017@gmail.com>
2025-02-18 10:48:35 +0800

    fix rest folder

---
+++ restaurant/order/models.py
@@ -0,0 +1,55 @@
+from distutils.sysconfig import customize_compiler
+
+from django.db import models
+
+# Create your models here.
+# Each table in the restaurant
+class Table(models.Model):
+    table_number = models.IntegerField(unique=True)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Table {self.table_number}"
+
+# Salad, Dishes, Soup, Desserts, Drinks
+class Category(models.Model):
+    category_title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.category_title
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # image = models.ImageField(upload_to='images/')
+    base_price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} , {self.category.category_title}"
+
+class Customization(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='customizations')
+    meat = models.CharField(max_length=50, choices=[('Beef', 'Beef'), ('Chicken', 'Chicken'), ('None', 'None')])
+    spice_level = models.CharField(max_length=50, choices=[('Low', 'Low'), ('Medium', 'Medium'), ('High', 'High')])
+    extra_cost = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return f'{self.item.name} ({self.meat} , {self.spice_level})'
+
+class UserOrder(models.Model):
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50, choices=[('Pending', 'Pending'), ('Completed', 'Completed')])
+
+    def __str__(self):
+        return f'Order {self.id} , Table {self.table.table_number} ({self.status})'
+
+class OrderItem(models.Model):
+    user_order = models.ForeignKey(UserOrder, on_delete=models.CASCADE)
+    customization= models.ForeignKey(Customization, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.customization.item.name} x {self.quantity} - Order {self.user_order.id}"
