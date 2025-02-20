b931c419e5c12404bcbd69fc1a9b7e8e341ce69e
BanyarSawThit <banyarsawthit1922017@gmail.com>
2025-02-20 18:08:27 +0800

    MVT updates

--- restaurant/order/urls.py
+++ restaurant/order/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.menu_list, name='menu_list'),
-    path("order/<int:table_number>/customization_page/", views.customization_page, name="customization_page"),
+    path('table/', views.table_select, name='table_select'),
+    path('table/<int:table_id>/', views.menu_page, name='menu_page'),
+    path("table/<int:table_id>/item/<int:pk>/", views.customization_page, name="customization_page"),
 ]
--- restaurant/order/views.py
+++ restaurant/order/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Table, Category, Item,  Customization, UserOrder, OrderItem
 
-def menu_list(request, table_number):
+def table_select(request):
+    tables = Table.objects.all()
+    return render(request, 'order/table_selection.html', {'tables': tables})
+def menu_page(request, table_id):
     menu_items = Item.objects.all()
-    return render(request, 'order/menu_page.html', {'menu_items': menu_items, "table_number": table_number})
+    return render(request, 'order/menu_page.html', {'menu_items': menu_items, 'table_id': table_id})
+
+def customization_page(request, table_id, pk):
+    menu_list = get_object_or_404(Item, pk=pk)
+    return render(request, 'order/customization_page.html', {'menu_list': menu_list})
