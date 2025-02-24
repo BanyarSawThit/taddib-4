35b916196cca3115a63612b6a6a623924efe04ba
BanyarSawThit <banyarsawthit1922017@gmail.com>
2025-02-24 17:35:46 +0800

    customization page

--- restaurant/order/views.py
+++ restaurant/order/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Table, Category, Item,  Customization, UserOrder, OrderItem
-
+from .forms import CustomizationForm
 def table_select(request):
     vtables = Table.objects.all()
     return render(request, 'order/table_selection.html', {'tables': vtables})
@@ -15,7 +15,17 @@ def menu_page(request, table_id):
 #     return render(request, 'order/customization_page.html', {'menu_list': menu_list})
 
 def customization_page(request, table_id, item_id):
-
-
+    form = CustomizationForm()
     item = get_object_or_404(Item, pk=item_id)
-    return render(request, 'order/customization_page.html', {'item':item,  'table_id': table_id})
+
+    if request.method == 'POST':
+        form = CustomizationForm(request.POST)
+        if form.is_valid():
+            customization = form.save(commit=False)
+            customization.item = item
+            customization.save()
+            return render(request, 'order/customization_page.html', {'item':item, 'table_id': table_id})
+        else:
+            form = CustomizationForm()
+
+    return render(request, 'order/customization_page.html', {'item':item, 'form': form, 'table_id': table_id})
