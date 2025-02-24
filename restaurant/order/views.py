7d96b23d98cd36029f7a945a92cbeba7634ccc44
BanyarSawThit <banyarsawthit1922017@gmail.com>
2025-02-24 17:35:46 +0800

    customization page

--- restaurant/order/views.py
+++ restaurant/order/views.py
@@ -1,40 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .models import Table, Category, Item, Customization, UserOrder, OrderItem
-
-
+from .models import Table, Category, Item,  Customization, UserOrder, OrderItem
+from .forms import CustomizationForm
 def table_select(request):
     vtables = Table.objects.all()
     return render(request, 'order/table_selection.html', {'tables': vtables})
-
-
 def menu_page(request, table_id):
-    # Fetch all categories
-    categories = Category.objects.all()
-    # Fetch the selected category if available
-    selected_category = request.GET.get('category', None)
-    if selected_category:
-        # Filter items by the selected category
-        menu_items = Item.objects.filter(category_id=selected_category)
-    else:
-        # If no category is selected, show all items
-        menu_items = Item.objects.all()
-    # Pass categories and selected category to the template
-    return render(request, 'order/menu_page.html', {
-        'menu_items': menu_items,
-        'categories': categories,
-        'table_id': table_id,
-        'selected_category': selected_category
-    })
     menu_items = Item.objects.all()
     return render(request, 'order/menu_page.html', {'menu_items': menu_items, 'table_id': table_id})
-
 
 # def customization_page(request, table_id, pk):
 #     menu_list = get_object_or_404(Item, pk=pk)
 #     return render(request, 'order/customization_page.html', {'menu_list': menu_list})
 
 def customization_page(request, table_id, item_id):
+    form = CustomizationForm()
     item = get_object_or_404(Item, pk=item_id)
-    return render(request, 'order/customization_page.html', {'item': item, 'table_id': table_id})
+
+    if request.method == 'POST':
+        form = CustomizationForm(request.POST)
+        if form.is_valid():
+            customization = form.save(commit=False)
+            customization.item = item
+            customization.save()
+            return render(request, 'order/customization_page.html', {'item':item, 'table_id': table_id})
+        else:
+            form = CustomizationForm()
+
+    return render(request, 'order/customization_page.html', {'item':item, 'form': form, 'table_id': table_id})
