876c65927f3ae665d3d0c9a9f3595c8a3fc66c35
Tin Nilar Soe <tinnilarsoe@my.jcu.edu.au>
2025-02-27 12:32:08 +0800

    menu html design

--- restaurant/order/urls.py
+++ restaurant/order/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('table/', views.table_select, name='table_select'),
+    path('', views.table_select, name='table_select'),
     path('table/<int:table_id>/', views.menu_page, name='menu_page'),
     path("table/<int:table_id>/item/<int:item_id>", views.customization_page, name="customization_page"),
 ]
