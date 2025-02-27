9151f4f15d46ebf9818b1b0c7c14a040ec6a4de5
BanyarSawThit <banyarsawthit1922017@gmail.com>
2025-02-27 12:06:09 +0800

    meat spicy update

--- restaurant/order/forms.py
+++ restaurant/order/forms.py
@@ -4,8 +4,4 @@ from .models import Customization
 class CustomizationForm(forms.ModelForm):
     class Meta:
         model = Customization
-        fields = ['meat', 'spice_level']
-        widgets = {
-            'meat': forms.RadioSelect(),
-            'spice_level': forms.RadioSelect(),
-        }
+        fields = ['meat', 'spicy_level']
--- restaurant/order/views.py
+++ restaurant/order/views.py
@@ -10,10 +10,6 @@ def menu_page(request, table_id):
     menu_items = Item.objects.all()
     return render(request, 'order/menu_page.html', {'menu_items': menu_items, 'table_id': table_id})
 
-# def customization_page(request, table_id, pk):
-#     menu_list = get_object_or_404(Item, pk=pk)
-#     return render(request, 'order/customization_page.html', {'menu_list': menu_list})
-
 def customization_page(request, table_id, item_id):
     form = CustomizationForm()
     item = get_object_or_404(Item, pk=item_id)
