bcf376c844982439abfd11ec5c5a0fa550aa59a7
BanyarSawThit <banyarsawthit1922017@gmail.com>
2025-02-27 12:06:09 +0800

    meat spicy update

--- restaurant/order/models.py
+++ restaurant/order/models.py
@@ -22,8 +22,10 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    # image = models.ImageField(upload_to='images/')
+    item_image = models.ImageField(default='supernova.jpg', blank=True)
     base_price = models.DecimalField(max_digits=5, decimal_places=2)
+    has_meat_options = models.BooleanField(default=True)
+    has_spicy_options = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.name} , {self.category.category_title}"
@@ -31,11 +33,11 @@ class Item(models.Model):
 class Customization(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='customizations')
     meat = models.CharField(max_length=50, choices=[('Beef', 'Beef'), ('Chicken', 'Chicken'), ('None', 'None')])
-    spice_level = models.CharField(max_length=50, choices=[('Low', 'Low'), ('Medium', 'Medium'), ('High', 'High')])
+    spicy_level = models.CharField(max_length=50, choices=[('Mild', 'Mild'), ('Medium', 'Medium'), ('High', 'High')])
     extra_cost = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
 
     def __str__(self):
-        return f'{self.item.name} ( meat-{self.meat} , spicy-{self.spice_level})'
+        return f'{self.item.name} ( meat-{self.meat} , spicy-{self.spicy_level})'
 
 class UserOrder(models.Model):
     table = models.ForeignKey(Table, on_delete=models.CASCADE)
