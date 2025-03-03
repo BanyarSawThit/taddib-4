4362230cd2d100abf6ff6221a1fe4c3e90ca5245
BanyarSawThit <banyarsawthit1922017@gmail.com>
2025-03-03 12:03:54 +0800

    category_nav_in_menu

--- restaurant/order/views.py
+++ restaurant/order/views.py
@@ -3,10 +3,32 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Table, Category, Item,  Customization, UserOrder, OrderItem
 from .forms import CustomizationForm
+from django.contrib import messages
 def table_select(request):
     vtables = Table.objects.all()
     return render(request, 'order/table_selection.html', {'tables': vtables})
 def menu_page(request, table_id):
+
+
+    # menu changes
+    # Fetch all categories
+    categories = Category.objects.all()
+    # Fetch the selected category if available
+    selected_category = request.GET.get('category', None)
+    if selected_category:
+        # Filter items by the selected category
+        menu_items = Item.objects.filter(category_id=selected_category)
+    else:
+        # If no category is selected, show all items
+        menu_items = Item.objects.all()
+    # Pass categories and selected category to the template
+    return render(request, 'order/menu_page.html', {
+        'menu_items': menu_items,
+        'categories': categories,
+        'table_id': table_id,
+        'selected_category': selected_category
+    })
+
     menu_items = Item.objects.all()
     return render(request, 'order/menu_page.html', {'menu_items': menu_items, 'table_id': table_id})
 
@@ -21,6 +43,9 @@ def customization_page(request, table_id, item_id):
             customization = form.save(commit=False)
             customization.item = item
             customization.save()
+
+            messages.success(request, "Item successfully added to cart!")
+
             return render(request, 'order/customization_page.html', {'item':item, 'form': form, 'table_id': table_id})
         else:
             form = CustomizationForm()
